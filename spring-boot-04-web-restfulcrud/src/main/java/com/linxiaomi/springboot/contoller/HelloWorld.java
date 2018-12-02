054ae06df824e840662e17a91465f73224273709
linxiaomi <155294446@qq.com>
2018-12-02 18:04:58 +0800

    add thymeleaf

---
+++ spring-boot-04-web-restfulcrud/src/main/java/com/linxiaomi/springboot/contoller/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.linxiaomi.springboot.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
+public class HelloWorld {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("hello", "你好");
+        map.put("users", Arrays.asList("大黄", "旺财", "小黑"));
+        return "success";
+    }
+}
