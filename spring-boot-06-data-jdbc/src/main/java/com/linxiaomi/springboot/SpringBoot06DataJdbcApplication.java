fbc02481d4e77f1d42a8318367569c03f3786ce4
linxiaomi <155294446@qq.com>
2019-01-01 17:39:56 +0800

    add druid datasource

---
+++ spring-boot-06-data-jdbc/src/main/java/com/linxiaomi/springboot/SpringBoot06DataJdbcApplication.java
@@ -0,0 +1,14 @@
+package com.linxiaomi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot06DataJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot06DataJdbcApplication.class, args);
+	}
+
+}
+
