79f12b0c05f50660d23e8af03ad4790839c661fc
linxiaomi <155294446@qq.com>
2019-01-05 23:13:09 +0800

    add cache annotation

--- springboot-cache/src/main/java/com/linxiaomi/cache/SpringbootCacheApplication.java
+++ springboot-cache/src/main/java/com/linxiaomi/cache/SpringbootCacheApplication.java
@@ -3,9 +3,11 @@ package com.linxiaomi.cache;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.linxiaomi.cache.mapper")
+@EnableCaching
 public class SpringbootCacheApplication {
 
 	public static void main(String[] args) {
