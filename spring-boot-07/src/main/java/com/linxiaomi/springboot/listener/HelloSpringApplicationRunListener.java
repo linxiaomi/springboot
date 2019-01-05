06d07bf9789d78fddcc92ce02aa316e5ed0f1c3c
linxiaomi <155294446@qq.com>
2019-01-05 17:48:50 +0800

    add user define listeners

---
+++ spring-boot-07/src/main/java/com/linxiaomi/springboot/listener/HelloSpringApplicationRunListener.java
@@ -0,0 +1,49 @@
+package com.linxiaomi.springboot.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class HelloSpringApplicationRunListener implements SpringApplicationRunListener {
+
+    public HelloSpringApplicationRunListener(SpringApplication application, String[] args) {
+
+    }
+
+    @Override
+    public void starting() {
+        System.out.println("SpringApplicationRunListener...starting");
+    }
+
+    @Override
+    public void environmentPrepared(ConfigurableEnvironment environment) {
+        Object object = environment.getSystemEnvironment().get("os.name");
+        System.out.println("SpringApplicationRunListener...environmentPrepared" + object);
+    }
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+        System.out.println("SpringApplicationRunListener...contextPrepared...");
+    }
+
+    @Override
+    public void contextLoaded(ConfigurableApplicationContext context) {
+        System.out.println("SpringApplicationRunListener...contextLoaded...");
+    }
+
+    @Override
+    public void started(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void running(ConfigurableApplicationContext context) {
+
+    }
+
+    @Override
+    public void failed(ConfigurableApplicationContext context, Throwable exception) {
+
+    }
+}
