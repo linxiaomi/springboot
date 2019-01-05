89e1c73b3aefa6e8533e1fc0adb679b6fce6fa0e
linxiaomi <155294446@qq.com>
2019-01-05 19:22:06 +0800

    add user define starter and starter test

---
+++ spring-boot-starter-test/src/main/java/com/linxiaomi/springboot/SpringBootStarterTestApplication.java
@@ -0,0 +1,14 @@
+package com.linxiaomi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterTestApplication.class, args);
+	}
+
+}
+
