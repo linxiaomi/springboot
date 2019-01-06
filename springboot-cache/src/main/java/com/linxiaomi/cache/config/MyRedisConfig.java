992ba0f01e80f68021a673be131d3355e5ec1dc6
linxiaomi <155294446@qq.com>
2019-01-06 16:51:53 +0800

    add redisCacheManager

--- springboot-cache/src/main/java/com/linxiaomi/cache/config/MyRedisConfig.java
+++ springboot-cache/src/main/java/com/linxiaomi/cache/config/MyRedisConfig.java
@@ -3,6 +3,7 @@ package com.linxiaomi.cache.config;
 import com.linxiaomi.cache.bean.Employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -20,5 +21,12 @@ public class MyRedisConfig {
         Jackson2JsonRedisSerializer<Employee> serializer = new Jackson2JsonRedisSerializer<Employee>(Employee.class);
         template.setDefaultSerializer(serializer);
         return template;
+    }
+
+    @Bean
+    public RedisCacheManager employeeCacheManager(RedisTemplate<Object, Employee> employeeRedisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(employeeRedisTemplate);
+        cacheManager.setTransactionAware(true);
+        return cacheManager;
     }
 }
