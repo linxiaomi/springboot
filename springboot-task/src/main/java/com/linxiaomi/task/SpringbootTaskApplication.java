9d855fc2e5e4a8bb5ed836791f83b5930d92da32
linxiaomi <155294446@qq.com>
2019-01-13 11:04:29 +0800

    add async annotation

---
+++ springboot-task/src/main/java/com/linxiaomi/task/SpringbootTaskApplication.java
@@ -0,0 +1,14 @@
+package com.linxiaomi.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTaskApplication.class, args);
+	}
+
+}
+
---
+++ springboot-task/src/main/java/com/linxiaomi/task/controller/AsyncController.java
@@ -0,0 +1,22 @@
+package com.linxiaomi.task.controller;
+
+import com.linxiaomi.task.service.AsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 开启异步注解功能
+@EnableAsync
+@RestController
+public class AsyncController {
+
+    @Autowired
+    AsyncService asyncService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        asyncService.hello();
+        return "success";
+    }
+}
