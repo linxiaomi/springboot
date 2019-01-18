b511861d701afeed3b6c1f0c60bfb1269fa94b41
linxiaomi <155294446@qq.com>
2019-01-18 18:07:57 +0800

    批量修改某文件夹下的文件名称

--- spring-boot-01-helloworld-quick/src/test/java/com/linxiaomi/springboot/SpringBoot01HelloworldQuickApplicationTests.java
+++ spring-boot-01-helloworld-quick/src/test/java/com/linxiaomi/springboot/SpringBoot01HelloworldQuickApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBoot01HelloworldQuickApplicationTests {
@@ -13,4 +15,36 @@ public class SpringBoot01HelloworldQuickApplicationTests {
 	public void contextLoads() {
 	}
 
+	/**
+	 * @Description:    批量修改某文件夹下的文件名称
+	 * @Author:         linxiaomi
+	 * @CreateDate:     2019/1/18 18:05
+	 * @UpdateUser:     linxiaomi
+	 * @UpdateDate:     2019/1/18 18:05
+	 * @UpdateRemark:   
+	 * @Version:        1.0
+	 * @return:         
+	 */
+	@Test
+	public void test() {
+		try {
+			// 要天剑的文件所在目录
+			String path = "";
+			// 要替换的文件名称
+			String replaceStr = "";
+			File file = new File(path);
+			if (file.exists()) {
+				File[] files = file.listFiles();
+				for (int i=0; i<files.length; i++) {
+					if (files[i].getName().contains(replaceStr)) {
+						File oldfile=new File(path+"\\"+files[i].getName());
+						File newfile=new File(path+"\\"+files[i].getName().replace(replaceStr, ""));
+						oldfile.renameTo(newfile);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
